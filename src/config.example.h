e19801dccd59b4936cce448e080b49162cf0e595
Markus Hofer <markus@brightlight.rocks>
2024-05-17 23:47:26 +1200

    added config example

---
+++ src/config.example.h
@@ -0,0 +1,4 @@
+// Rename this file to config.h after filling in the values below
+#define WIFI_NAME "FILL IN YOUR WIFI NAME HERE"
+#define WIFI_PASSWORD "FILL IN YOUR WIFI PASSWORD HERE"
+#define OPENAI_API_KEY "FILL IN YOUR OPENAI API KEY HERE"
